1f8a82bc7e90ddf0ed079f8ef1f304979b7a5708
Onkar Vatsa <onkarvatsa@gmail.com>
2023-08-29 13:36:19 +0530

    1st commit on second repo

--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -5,13 +5,13 @@ const MapComponent = () => {
   return (
     <div className="relative h-full">
       <div className="overflow-hidden absolute inset-0">
-        {/* <iframe
+        <iframe
           className="w-full h-full rounded-lg"
           frameBorder="0"
           scrolling="no"
           src="https://maps.google.com/maps?width=600&height=400&hl=en&q=gurgaon&t=&z=14&ie=UTF8&iwloc=B&output=embed"
           title="Google Map"
-        ></iframe> */}
+        ></iframe>
       </div>
     </div>
   );
