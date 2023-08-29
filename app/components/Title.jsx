f84ed9df4311cd6bc85f02cc21a1959c3133b174
Onkar Vatsa <onkarvatsa@gmail.com>
2023-08-29 18:08:53 +0530

    full optimized code

--- app/components/Title.jsx
+++ app/components/Title.jsx
@@ -4,7 +4,7 @@ import { Bounce } from "react-awesome-reveal";
 const Title = ({ heading }) => {
   return (
     <div className="title text-[#000] md:text-3xl w-[350px] py-5 text-center mx-auto md:my-5 2 capitalise rotating-border text-2xl font-semibold font-'Josefin Sans', sans-serif">
-      <Bounce duration={2000}>{heading}</Bounce>
+      <Bounce>{heading}</Bounce>
     </div>
   );
 };
